8363428fff143cdb0c090ef636e0fc20dadc767d
Jonas Américo <jjsamerico@gmail.com>
2018-06-16 17:58:56 -0300

    Ajuste front máquina de turing

--- public/js/separaTudo.js
+++ public/js/separaTudo.js
@@ -11,14 +11,29 @@ class Estados {
     adicionarEstado(estado) {
         this.arrayEstados.push(estado);
     }
+    get returnArrayEstados(){
+        var obj = {"array": this.arrayEstados};
+        return obj;
+    }
+
+
+
 }
 
 
 var estados = new Estados();
 
 function capturaEstado() {
+    //Feito para conseguir add mais linha no textarea
+    var objTextArea = document.getElementById('txtArea');
+    while (objTextArea.scrollHeight > objTextArea.offsetHeight){objTextArea.rows += 1;}
+
     //Caputura o valor do estado que vem do html
     var estadoCapturado = document.getElementById('estado').value;
+    var txtAreaCapturado = document.getElementById('txtArea').value;
+
+    document.getElementById('txtArea').value = txtAreaCapturado+estadoCapturado+"\n";
+
     //Verifica se nao é espaco vazil
     if (estadoCapturado.trim() !== "") {
 
@@ -30,6 +45,38 @@ function capturaEstado() {
     } else {
         alert("Errou!")
     }
+
+}
+
+function enviarJson(){
+    // $.post({
+    //     url: 'http://localhost/maquina-de-turing/teste',
+    //     data: estados,
+    //     // type: "POST"
+    // }).done((response) => {
+    //     alert("Deu certo o json!");
+    // }).fail((response) => {
+    //     alert("Deu errado o json!");
+    // });
+    $.ajax({
+        type: "POS",
+        data: { "teste": "teste" },
+        url: "http://localhost/maquina-de-turing/teste",
+        success: function(data) {
+            console.log("teste");
+        }
+    });
+
+
+
+
+    // var testee = { "testando" :"testando"}
+    // console.log(testee);
+    // $.post( "test.php", { name: "John", time: "2pm" } );
+
+
+
+
 
 }
 
--- public/js/tela-inicial.js
+++ public/js/tela-inicial.js
@@ -1,3 +1,5 @@
+var posicaoGlobal = 0;
+var relogioVelocidade = 1000;
 
 
 
@@ -13,6 +15,19 @@ function criaDiv(variavel) {
 function chamaDiv(){
     for(var i = 0; i < 20;i++){
         criaDiv(i);
+
+function criaDiv(variavel,teste1) {
+    if (variavel === 0) {
+        $(document.getElementById('lineBox')).append('<div id="box' + variavel + '" class="box">'+ teste1 +'</div>');
+    } else {
+        $(document.getElementById('lineBox')).append('<div id="box' + variavel + '" class="box">' + teste1 + '</div>');
+    }
+}
+
+function chamaDiv() {
+    var teste = 'XXXXXX_XXXXXX'
+    for (var i = 0; i < teste.length; i++) {
+        criaDiv(i,teste[i]);
     }
 }
 
@@ -49,3 +64,64 @@ function iniciaFita() {
 
 
 }
+function animacaoFita(i) {
+    $(document.getElementById('box' + i)).addClass('animacao-jquery');
+}
+function moverDireita() {
+    var posicaoAtualDireita = posicaoGlobal;
+    var removerDireitaAnterior = posicaoGlobal-1;
+    console.log("movendo direita"+posicaoGlobal)
+    $(document.getElementById('box' + removerDireitaAnterior)).removeClass('animacao-jquery');
+
+    $(document.getElementById('box' + posicaoAtualDireita)).addClass('animacao-jquery');
+}
+
+function moverEsquerda() {
+    var posicaoEsquerdaAtual = posicaoGlobal;
+    var removerEsquerdaAnterior = posicaoGlobal+1;
+    $(document.getElementById('box' + removerEsquerdaAnterior)).removeClass('animacao-jquery');
+    $(document.getElementById('box' + posicaoEsquerdaAtual)).addClass('animacao-jquery');
+}
+
+function alteraValor(alteracaoAtual) {
+    var div = document.getElementById('box'+posicaoGlobal);
+    div.innerHTML = alteracaoAtual;
+}
+
+function iniciaFita() {
+    animacaoFita(0);
+    var fitaMontada = "";
+    var fita = ['J1','o1','n1','a1','s1','!0','!0','!0','!0','!0'];
+    var tamanhoFita = fita.length;
+
+    for (var i = 0; i < tamanhoFita; i++) {
+        var acao = fita[i];
+        var tamanhoAcao = acao.length;
+        for (var j = 0; j < tamanhoAcao; j++) {
+            var aux = fitaMontada+acao[j];
+            fitaMontada = aux;
+        }
+    }
+    for (var i = 0; i < fitaMontada.length; i ++){
+        if(fitaMontada[i] == '0'){
+            (function (ind) {
+                setTimeout(function () {posicaoGlobal--; moverEsquerda(); }, 1000 + (50 * ind));
+            })(i);
+        }
+        else if(fitaMontada[i] == '1'){
+            (function (ind) {
+                setTimeout(function () {posicaoGlobal++; moverDireita(); }, 1000 + (50 * ind));
+            })(i);
+        }else{
+            (function (ind) {
+                setTimeout(function () { alteraValor(fitaMontada[ind]); }, 1000 + (50 * ind));
+            })(i);
+        }
+    }
+
+}
+
+// // if()
+// (function (ind) {
+//     setTimeout(function () { moverDireita(ind); }, 1000 + (1000 * ind));
+// })(i);
